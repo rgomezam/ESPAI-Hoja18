4665a31bd419a04b61f500348fa1d99b9a5cc89f
Raquel Gomez <raquel.gomez@cern.ch>
2015-08-29 01:36:33 +0200

    Create Material.cpp

---
+++ ejercicio7/Material.cpp
@@ -0,0 +1,20 @@
+/*
+ * Material.cpp
+ *
+ *  Created on: Aug 28, 2015
+ *      Author: raquel
+ */
+
+#include "Material.h"
+
+Material::Material(string titulo, string autor) {
+
+	this->titulo = titulo;
+	this ->autor= autor;
+
+}
+
+Material::~Material() {
+	// TODO Auto-generated destructor stub
+}
+
